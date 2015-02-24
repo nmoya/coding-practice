c3d7662c613a8e43bb5190db0c53b9d445cc30b7
nmoya <nikolasmoya@gmail.com>
2015-02-24 15:41:48 -0300

    Comments added for the sorting algorithms

--- Minimal/sorting.py
+++ Minimal/sorting.py
@@ -3,6 +3,8 @@ import random
 
 
 def isSorted(array):
+    ''' Checks if the array is sorted. An array is sorted if :
+        pass for all i, a[i] < a[j]. Complexity: O(n) '''
     i = 0
     while i < len(array) - 1:
         if array[i] > array[i + 1]:
@@ -12,13 +14,19 @@ def isSorted(array):
 
 
 def shuffle(array):
-  i = 0
-  while i < len(array):
-      j = random.randrange(0, len(array))
-      array[i], array[j] = array[j], array[i]
-      i += 1
+    ''' The numbers before i are shuffled. For each i, select another position
+    of the array and swap with i. Complexity: O(n). '''
+    i = 0
+    while i < len(array):
+        j = random.randrange(0, len(array))
+        array[i], array[j] = array[j], array[i]
+        i += 1
+
 
 def merge(array, aux, lo, mid, hi):
+    ''' Merge procedure called inside mergesort. Given a sorted left and
+    right array, merge both arrays in a new array of size 
+    len(left) + len(right) '''
     k = lo
     while k <= hi:
         aux[k] = array[k]
@@ -41,7 +49,10 @@ def merge(array, aux, lo, mid, hi):
             i += 1
         k += 1
 
+
 def _mergesort(array, aux, lo, hi):
+    ''' Recursive function that splits the array in half each call and
+    recursively sorts each half.'''
     if hi <= lo:
         return
     else:
@@ -52,11 +63,15 @@ def _mergesort(array, aux, lo, hi):
 
 
 def mergesort(array):
+    ''' Just the interface to call _mergesort. Mergesort is O(n log n) time and
+    O(n) space. '''
     aux = [0 for i in range(len(array))]
     _mergesort(array, aux, 0, len(array) - 1)
 
 
 def insertionsort(array):
+    ''' All the numbers before i are sorted. For each i, traverse with j from 
+    i + 1 until the end. If j < i, swap. That's it. Complexity: O(n^2). '''
     i = 0
     while i < len(array) - 1:
         j = i + 1
@@ -66,10 +81,15 @@ def insertionsort(array):
             j += 1
         i += 1
 
+
 def selectionsort(array):
+    ''' Very similar to insertionsort. All the numbers before i are sorted. 
+    For each i, traverse with j from i + 1 until the end searching for the 
+    minimum value. Swap the minimum value with i only once at the end.
+    Complexity O(n^2). Usually worst than insertionsort. '''
     i = 0
     while i < len(array) - 1:
-        _min, _min_pos = array[i+1], i+1
+        _min, _min_pos = array[i + 1], i + 1
         j = i + 2
         while j < len(array):
             if array[j] < _min:
@@ -91,4 +111,3 @@ if __name__ == "__main__":
     print isSorted(tmp)
     selectionsort(tmp)
     print isSorted(tmp)
-
