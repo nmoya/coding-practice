a7853b29c02ace53a902addbb8cc5a6037659a7c
nmoya <nikolasmoya@gmail.com>
2015-04-20 16:33:15 -0300

    Hanoi cleaner

--- Algorithms/hanoi.py
+++ Algorithms/hanoi.py
@@ -1,7 +1,13 @@
-def hanoi(ndisks, startPeg=1, endPeg=3):
-    if ndisks:
-        hanoi(ndisks-1, startPeg, 6-startPeg-endPeg)
-        print "Move disk %d from peg %d to peg %d" % (ndisks, startPeg, endPeg)
-        hanoi(ndisks-1, 6-startPeg-endPeg, endPeg)
- 
-hanoi(ndisks=5)
+def hanoi(n, source, helper, target):
+	if n > 0:
+		hanoi(n-1, source, target, helper)
+		if source:
+			target.append(source.pop())
+		hanoi(n-1, helper, source, target)
+
+
+source = [5, 4, 3, 2, 1]
+helper = []
+target = []
+hanoi(len(source), source, helper, target)
+print source, helper, target
