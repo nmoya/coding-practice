c62e7f57b19ca8df7f6e720f7889dbfeaba3c2b3
nmoya <nikolasmoya@gmail.com>
2015-04-20 18:20:08 -0300

    Statistical improvements on measuring the change of victory in a bingo match

--- Algorithms/bingo.py
+++ Algorithms/bingo.py
@@ -3,11 +3,6 @@ import sys
 import random
 
 
-WIN_CONDITION = "Row" # "Row", "Column", "Diagonal", "Both_Diagonals", "Full"
-TICKET_SIZE = 5
-MAX_VALUE = 75    # Must be multiple of TICKET_SIZE
-
-
 def rand_without_repetition(_min, _max, n):
 	if (_max - _min)+1 >= n:
 		tmp = range(_min, _max+1, 1)
@@ -49,8 +44,9 @@ class Matrix():
 
 
 class Ticket():
-	def __init__(self, size):
+	def __init__(self, size, max_value):
 		self.size = size
+		self.max_value = max_value
 		self.ticket = Matrix(size, size)
 		self.gen_ticket()
 		self.ticket.set_middle(-1)
@@ -73,12 +69,12 @@ class Ticket():
 		self.label.set_middle(1)
 
 	def gen_ticket(self):
-		step = int(MAX_VALUE / TICKET_SIZE)
+		step = int(self.max_value / self.size)
 		for i in range(self.ticket.ysize):
-			# print (i*step)+1, (i*step)+step, TICKET_SIZE
+			# print (i*step)+1, (i*step)+step, self.size
 			self.ticket.set_row(i, rand_without_repetition((i*step)+1,
 														   (i*step)+step,
-														   TICKET_SIZE))
+														   self.size))
 		self.ticket.transpose()
 
 	def mark_number(self, number):
@@ -91,13 +87,13 @@ class Ticket():
 	def check_ticket(self, win_condition):
 		if win_condition == "Row":
 			for row in self.label.data:
-				if sum(row) == TICKET_SIZE:
+				if sum(row) == self.size:
 					return True
 			return False
 		elif win_condition == "Column":
 			self.label.transpose()
 			for row in self.label.data:
-				if sum(row) == TICKET_SIZE:
+				if sum(row) == self.size:
 					self.label.transpose()
 					return True
 			self.label.transpose()
@@ -106,7 +102,7 @@ class Ticket():
 			_sum = 0
 			for i in range(self.label.ysize):
 				_sum += self.label.data[i][i]
-			if _sum == TICKET_SIZE:
+			if _sum == self.size:
 				return True
 			else:
 				return False
@@ -117,7 +113,7 @@ class Ticket():
 				_sum1 += self.label.data[i][i]
 				_sum2 += self.label.data[i]\
 										[self.label.ysize-(i+1)]
-			if _sum1 == TICKET_SIZE and _sum2 == TICKET_SIZE:
+			if _sum1 == self.size and _sum2 == self.size:
 				return True
 			else:
 				return False
@@ -125,20 +121,20 @@ class Ticket():
 			_sum = 0
 			for line in self.label.data:
 				_sum += sum(line)
-			if _sum == TICKET_SIZE ** 2:
+			if _sum == self.size ** 2:
 				return True
 			else:
 				return False
 
 
 class Player():
-	def __init__(self, _id, nbr_tickets):
+	def __init__(self, _id, nbr_tickets, ticket_size, max_value):
 		self.id = _id
 		self.wins = 0
 		self.nbr_tickets = nbr_tickets
 		self.tickets = []
 		for i in range(nbr_tickets):
-			self.tickets.append(Ticket(TICKET_SIZE))
+			self.tickets.append(Ticket(ticket_size, max_value))
 
 	def __repr__(self):
 		output = "Player # %d\n" % (self.id)
@@ -170,9 +166,11 @@ class Event():
 		self.nbr_rounds                 = kargs.get("nbr_rounds", 30)
 
 		self.players = []
-		self.players.append(Player(1, self.nbr_tickets_special_player))
+		self.players.append(Player(1, self.nbr_tickets_special_player,
+							self.ticket_size, self.max_value))
 		for i in range(2, self.nbr_players):
-			self.players.append(Player(i+2, self.avg_tickets_per_player))
+			self.players.append(Player(i+2, self.avg_tickets_per_player,
+								self.ticket_size, self.max_value))
 
 		self.winner = self.players[0]
 
@@ -184,7 +182,8 @@ class Event():
 		return output
 
 	def tiebreak(self, round_winners):
-		tie_break = rand_without_repetition(1, MAX_VALUE, len(round_winners))
+		tie_break = rand_without_repetition(1, self.max_value,
+											len(round_winners))
 		_max = max(tie_break)
 		_max_idx = -1
 		for idx, num in enumerate(tie_break):
@@ -194,8 +193,11 @@ class Event():
 		return round_winners[idx]
 
 	def simulate(self):
+		special_player_won = False
+		avg_wins = 0
 		for rnd in range(self.nbr_rounds):
-			numbers = rand_without_repetition(1, MAX_VALUE, MAX_VALUE)
+			numbers = rand_without_repetition(1, self.max_value,
+											  self.max_value)
 			round_winners = []
 			for numb in numbers:
 				for p in self.players:
@@ -210,17 +212,39 @@ class Event():
 			for player in self.players:
 				player.clear()
 
-		print self
-		sorted_players = sorted(self.players, reverse=True, key=lambda p: p.wins)
-		for p in sorted_players:
-			if p.wins != 0:
-				print "Player # %d: %f" % (p.id, p.wins / self.nbr_rounds)
+		if self.winner.id == 1:
+			special_player_won = True
+
+		return special_player_won, self.players[0].wins
+		# print self
+		# sorted_players = sorted(self.players, reverse=True, key=lambda p: p.wins)
+		# for p in sorted_players:
+		# 	if p.wins != 0:
+		# 		print "Player # %d: %f" % (p.id, p.wins / self.nbr_rounds)
 
 
 
 if __name__ == "__main__":
-	e = Event(id=1, win_condition="Full", nbr_players=30,
-			  nbr_tickets_special_player=20, avg_tickets_per_player=2,
-			  nbr_rounds=15)
+	wins = 0
+	rounds = 0
+	win_conditions = ["Row", "Column", "Diagonal", "Both_Diagonals", "Full"]
+	number_of_events = 100
+	nbr_rounds = 15
+	for i in range(1, number_of_events+1):
+		if i % 10 == 0:
+			print "Simulating event # %d" % (i)
+		e = Event(id=i, win_condition=random.choice(win_conditions),
+				  nbr_players=30, nbr_tickets_special_player=20,
+				  avg_tickets_per_player=2, nbr_rounds=nbr_rounds)
 
-	e.simulate()
+		aux_wins, aux_rounds = e.simulate()
+		if aux_wins:
+			wins += 1
+		rounds += aux_rounds
+
+	print "-----------------------------------------"
+	print "Special player was first place in %.2f p" % ((wins/number_of_events) * 100)
+	print "Change to win a round: %.2f p" % ((rounds / (number_of_events * nbr_rounds)) *100)
+	print "Expected to win %.2f rounds of %d" % (rounds / number_of_events, nbr_rounds)
+	print "-----------------------------------------"
+
